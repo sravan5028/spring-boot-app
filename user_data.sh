1a3107a8c1c031a1236ebb209a4f388632735d55
sravan5028 <39333120+sravan5028@users.noreply.github.com>
2019-03-28 09:32:42 +0530

    Update user_data.sh

--- user_data.sh
+++ user_data.sh
@@ -2,5 +2,5 @@
 sudo yum update -y
 sudo yum remove *java* -y
 sudo yum install wget java-1.8.0-openjdk java-1.8.0-openjdk-devel -y
-wget http://52.66.205.227:8081/nexus/service/local/repositories/maven_spring_boot/content/com/spring-boot-aop/0.0.1/spring-boot-aop-0.0.1.jar
+wget http://172.31.86.70:8081/nexus/content/repositories/maven_release/com/javainuse/spring-boot-aop/0.0.1/spring-boot-aop-0.0.1.jar
 nohup java -jar spring-boot-aop-0.0.1.jar &
